357c7fbab014862f77a22e86f3e103886ec38660
KaiDove1 <ybr8ff@virginia.edu>
2023-11-23 05:40:36 -0500

    Initialize git repository, set up React environment, and create Header component in JSX.

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header classname = "header">
+        <nav classname = "nav container">
+            <a href = "index.html" classname = "nav__logo">Kai</a>  
+
+            <div classname = "nav__menu">
+                <ul classname = "nav__list grid">
+                    <li clasname = "nav__item">
+                        <a href = "#home" classname = "nav__link active-link">
+                            <i></i>Home
+                        </a>
+                    </li>
+                    <li clasname = "nav__item">
+                        <a href = "#home" classname = "nav__link active-link">
+                            <i></i>Home
+                        </a>
+                    </li>
+                    <li clasname = "nav__item">
+                        <a href = "#home" classname = "nav__link active-link">
+                            <i></i>Home
+                        </a>
+                    </li>
+                    <li clasname = "nav__item">
+                        <a href = "#home" classname = "nav__link active-link">
+                            <i></i>Home
+                        </a>
+                    </li>
+                    <li clasname = "nav__item">
+                        <a href = "#home" classname = "nav__link active-link">
+                            <i></i>Home
+                        </a>
+                    </li>
+                    <li clasname = "nav__item">
+                        <a href = "#home" classname = "nav__link active-link">
+                            <i></i>Home
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        
+
+        </nav>
+
+
+    </header>
+  )
+}
+
+export default Header
